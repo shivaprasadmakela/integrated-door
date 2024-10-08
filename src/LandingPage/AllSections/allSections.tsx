cbcc60bf82f89b9bbae5768585d438d5f2b9fef5
shiva prasad m <shivaprasadmakela@gmail.com>
2024-10-08 11:05:50 +0530

    completed top section

---
+++ src/LandingPage/AllSections/allSections.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TopSection from "../TopSection/topSection";
+
+function AllSections() {
+  return (
+    <React.Fragment>
+      <TopSection />
+    </React.Fragment>
+  );
+}
+
+export default AllSections;
